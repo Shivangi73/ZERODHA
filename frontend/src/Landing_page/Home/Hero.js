f952ee631cd1fdf02d2f07bcb1186fe1a994ced4
Shivangi Gupta <guptashivangi0315@gmail.com>
2026-04-20 01:12:03 +0530

    Fix final support hero links

--- frontend/src/Landing_page/Home/Hero.js
+++ frontend/src/Landing_page/Home/Hero.js
@@ -1,14 +1,27 @@
 import React from 'react';
+
 function Hero() {
-    return ( <div className='Container p-5 mb-5'>
-        <div className='row text-center'>
-     <img src='media/images/homeHero.png' alt='Hero image' className='mb-5'/>
-     <h1 className='mt-5'>Invest in Everything</h1>
-     <p>Online Platform to invest in stocks,derivatives,mutual funds,and more</p>
-     <button  className='p-2 btn btn-primary fs-5 mb-5' style={{width:"20%",margin:"0 auto"}}>Signup Now</button>
-        </div>
+  return (
+    <div className='Container p-5 mb-5'>
+      <div className='row text-center'>
+        <img
+          src='media/images/homeHero.png'
+          alt='hero'
+          className='mb-5'
+        />
+
+        <h1 className='mt-5'>Invest in Everything</h1>
+        <p>Online Platform to invest in stocks, derivatives, mutual funds, and more</p>
+
+        <button
+          className='p-2 btn btn-primary fs-5 mb-5'
+          style={{ width: "20%", margin: "0 auto" }}
+        >
+          Signup Now
+        </button>
+      </div>
     </div>
-        );
+  );
 }
 
 export default Hero;
